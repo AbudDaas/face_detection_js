2eb088b29ad52598bb575ecac093ce2099cd3fbb
Abud <abdulrazakdaas0@gmail.com>
2023-07-23 14:18:20 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="main">
+
+    </div>
+</template>
+  
+  <script>
+  import { store } from "./js/store.js";
+
+
+  export default{
+    name: "App",
+
+  };
+  </script>
+  
+  <style>
+  /* @import "./style/css/App.css"; */
+  </style>
